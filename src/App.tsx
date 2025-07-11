fbcea4b82af0634af0fb4e6ae71f72d554e5d956
mmadiee <demimanipolo222@gmail.com>
2025-07-11 13:46:25 +0800

    add Tabs component and integrate it into the App component; include custom font styles in CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+
+//Components
 import Header from './components/Header';
+import Tab from './components/Tabs';
 
 const App = () => {
     return (
         <>
             <Header></Header>
+            <Tab></Tab>
         </>
     )
 }
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Tab = () => {
+    return (
+        <div className="flex justify-between mx-20">
+            <h1 className="text-neutral-900 text-3xl font-bold">Extensions List</h1>
+            <div className="flex justify-between">
+                <button className="rounded-full py-2 px-4 mx-1 border border-neutral-300 bg-white shadow-sm text-neutral-900 hover:bg-neutral-100 hover:text-neutral-700 hover:shadow-none focus:ring-2 focus:ring-red-500 active:bg-red-500 active:text-white active:shadow-none transition ease-out duration-200">All</button>
+                <button className="rounded-full py-2 px-4 mx-1 border border-neutral-300 bg-white shadow-sm text-neutral-900 hover:bg-neutral-100 hover:text-neutral-700 hover:shadow-none focus:ring-2 focus:ring-red-500 active:bg-red-500 active:text-white active:shadow-none transition ease-out duration-200">Active</button>
+                <button className="rounded-full py-2 px-4 mx-1 border border-neutral-300 bg-white shadow-sm text-neutral-900 hover:bg-neutral-100 hover:text-neutral-700 hover:shadow-none focus:ring-2 focus:ring-red-500 active:bg-red-500 active:text-white active:shadow-none transition ease-out duration-200">Inactive</button>
+            </div>
+            
+        </div>
+    )
+} 
+
+export default Tab;
