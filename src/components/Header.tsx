7fe14ab838307e54102b34431051e5e0447d3760
mmadiee <demimanipolo222@gmail.com>
2025-07-11 13:58:07 +0800

    enhance Header component with hover and focus styles; add background gradient to HTML

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ import moon from '../../assets/images/icon-moon.svg';
 
 const Header = () => {
     return (
-        <header className="bg-neutral-0 p-4 shadow-md rounded-xl mx-20 my-10 flex justify-between items-center border border-neutral-200">
+        <header className="bg-neutral-0 p-4 shadow-md rounded-xl mx-20 my-10 flex justify-between items-center border border-neutral-200 ">
             <img src={logo} alt="Logo" />
-            <button className="bg-neutral-100 p-3 rounded-lg"><img src={moon} alt="Night Mode"/></button>
+            <button className="bg-neutral-100 p-3 rounded-lg hover:bg-neutral-400 hover:text-neutral-700 transition ease-out duration-200 focus:ring-2 focus:ring-red-500"><img src={moon} alt="Night Mode"/></button>
         </header>
     );
 };
