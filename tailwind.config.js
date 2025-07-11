bdc47b5876a177e853da3f08cce57715e21792a0
mmadiee <demimanipolo222@gmail.com>
2025-07-11 11:36:56 +0800

    add TypeScript declaration for SVG modules and extend Tailwind CSS configuration with custom colors, gradients, and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          900: 'hsl(227, 75%, 14%)',
+          800: 'hsl(226, 25%, 17%)',
+          700: 'hsl(225, 23%, 24%)',
+          600: 'hsl(226, 11%, 37%)',
+          300: 'hsl(0, 0%, 78%)',
+          200: 'hsl(217, 61%, 90%)',
+          100: 'hsl(0, 0%, 93%)',
+          0: 'hsl(200, 60%, 99%)',
+        },
+        red: {
+          400: 'hsl(3, 86%, 64%)',
+          500: 'hsl(3, 71%, 56%)',
+          700: 'hsl(3, 77%, 44%)',
+        },
+      },
+      backgroundImage: {
+        'light-gradient': 'linear-gradient(180deg, #EBF2FC 0%, #EEF8F9 100%)',
+        'dark-gradient': 'linear-gradient(180deg, #040918 0%, #091540 100%)',
+      },
+      fontFamily: {
+        sans: ['Noto Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
