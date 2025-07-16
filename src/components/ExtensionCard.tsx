e48d0d059cb55242e37c8146b6b83b28acd294e2
mmadiee <demimanipolo222@gmail.com>
2025-07-16 14:23:25 +0800

    refactor ExtensionCard layout for improved flexibility and responsiveness

--- src/components/ExtensionCard.tsx
+++ src/components/ExtensionCard.tsx
@@ -13,7 +13,7 @@ const ExtensionCard: React.FC<ExtensionProps> = ({ id, name, description, logo, 
     const logoPath = logo;
 
     return (
-        <div className="rounded-2xl bg-neutral-0 shadow-md border border-neutral-200 p-4">
+        <div className="rounded-2xl bg-neutral-0 shadow-md border border-neutral-200 p-4 flex flex-col">
             <div className="flex justify-around mb-10 items-start gap-3">
                 <img src={logoPath} alt={`${name} logo`} className="w-12 h-12" />
                 <div>
@@ -21,7 +21,7 @@ const ExtensionCard: React.FC<ExtensionProps> = ({ id, name, description, logo, 
                     <p className="text-neutral-600 text-sm">{description}</p>
                 </div>
             </div>
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center mt-auto">
                 <button className="py-1 px-3 bg-neutral-0 text-neutral-800 border border-neutral-400 rounded-full hover:bg-red-700 hover:text-neutral-0 transition duration-200 focus:ring-2 focus:ring-red-500">Remove</button>
                 <button
                     onClick={() => onToggleActive(id)}
